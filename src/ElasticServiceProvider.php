fc72cc29a9028e88eaa5db45d44dccc0d80ca4c2
jorgeortegaceja <33214280+jorgeortegaceja@users.noreply.github.com>
2021-06-10 15:32:13 -0500

    feacture: settings

---
+++ src/ElasticServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+namespace Elastic;
+
+use Illuminate\Support\ServiceProvider;
+
+class ClassName extends ServiceProvider
+{
+    public function register()
+    {
+        // Add database driver.
+        $this->app->resolving('db', function ($db) {
+            $db->extend('elastic', function ($config, $name) {
+                $config['name'] = $name;
+                return new Connection($config);
+            });
+        });
+
+        // Add connector for queue support.
+        // $this->app->resolving('queue', function ($queue) {
+        //     $queue->addConnector('mongodb', function () {
+        //         return new MongoConnector($this->app['db']);
+        //     });
+        // });
+    }
+}
